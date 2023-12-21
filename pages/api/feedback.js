476f20fd08aed443fd7ff64844580d6d98939252
AshishCoreProject <ashishbarthwal1799@gmail.com>
2023-12-21 17:11:47 +0530

    Adding Backend code with API routes

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,4 +1,26 @@
+import fs from "fs";
+import path from "path";
+
 function handler(req, res) {
-  res.status(200).json({ message: "This Works" });
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedbackText,
+    };
+
+    // store that in database or in a file
+    const filepath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filepath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filepath, JSON.stringify(data));
+    res.status(201).json({ message: "Success!", feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: "This Works" });
+  }
 }
 export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,49 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import { useRef } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredInput = feedbackInputRef.current.value;
+    fetch(); // {email: 'test@test.com , text:'some feedback text'}
+  }
   return (
     <>
       <h1 className="p-8 text-4xl">API routes</h1>
+      <div className="pl-8">
+        <form onSubmit={submitFormHandler} className="bg-yellow-200">
+          <div className="py-4">
+            <label className="mr-3" htmlFor="email">
+              Your Email Address
+            </label>
+            <input
+              ref={emailInputRef}
+              className="border border-orange-400 "
+              type="email"
+              id="email"
+            />
+          </div>
+          <div className="py-4">
+            <label className="mr-3" htmlFor="feedback">
+              Your feedback
+            </label>
+            <textarea
+              ref={feedbackInputRef}
+              className="border border-orange-400 "
+              id="feedback"
+              rows="5"
+            ></textarea>
+          </div>
+          <button className="p-2 bg-orange-400 rounded-lg">
+            Send Feedback
+          </button>
+        </form>
+      </div>
     </>
   );
 }
